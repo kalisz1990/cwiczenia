e319af3a4ea8d7c24632bde1cae0844eb9b71ea5
Paweł <kaliszewskipawel@o2.pl>
2017-12-10 18:51:40 +0100

    dziedziczenie, bez interfejsów

--- Figury/Figury.java
+++ Figury/Figury.java
@@ -7,79 +7,83 @@ package Figury;
 //        boki rombu pitagorasem
 
 import java.sql.SQLOutput;
-import java.util.Scanner;
 
 public class Figury {
 
-    private double bok1;
-    private double bok2;
-    private double bok3;
-    private double pole;
-    private double obwod;
+    double bok1;
+    double bok2;
+    double bok3;
+    double przekatna1;
+    double przekatna2;
+    double pole;
+    double obwod;
 
-    public Figury(int bok1, int bok2, int bok3)
-    {
-        //trojkat
+    public Figury (int bok1, int bok2, int bok3){
         this.bok1 = bok1;
         this.bok2 = bok2;
         this.bok3 = bok3;
+    }
+
+    public Figury (int przekatna1, int przekatna2) {
+        this.przekatna1 = przekatna1;
+        this.przekatna2 = przekatna2;
+    }
+
+    public static void main (String [] args)
+
+    {
+        Trojkat trojkat = new Trojkat(3,4,5);
+        System.out.println(trojkat.obwod());
+        System.out.println(trojkat.pole());
+        System.out.println();
+
+        Romb romb = new Romb(3,4);
+        System.out.println(romb.pole());
+        System.out.println(romb.bokRombu());
+    }
+}
+
+class Trojkat extends Figury {
+
+    public Trojkat(int bok1, int bok2, int bok3) {
+        super(bok1, bok2, bok3);
 
         if ((Math.abs(bok2-bok3)<bok1)&&(bok1<bok2+bok3))
         {
             System.out.println("To jest trojkat.");
-            System.out.println("Obwod trojkata: " + obwodTrojkata() + "\nPole trojkata: " + poleTrojkata());
         }
         else
             System.out.println("Te odcinki nie utworza trojkata.");
     }
 
-    public Figury(int bok1, int bok2)
-    {
-        //romb
-        this.bok1 = bok1;
-        this.bok2 = bok2;
-        System.out.println("to jest romb.");
-        System.out.println("Obwod rombu: " + obwodRombu() + "\nPole rombu: " + poleRombu());
-        System.out.println("Bok rombu: "+ bokRombu());
-
-    }
-    double obwodRombu()
-    {
-        obwod = bokRombu()*4;
-        return obwod;
-    }
-
-    double poleRombu()
-    {
-        pole = (bok1*bok2)/2;
-        return pole;
-    }
-
-    double bokRombu()
-    {
-        double bokRobu;
-        bokRobu = Math.sqrt(Math.pow((bok1/2),2) + Math.pow((bok2/2),2));
-        return bokRobu;
-    }
-
-    double obwodTrojkata()
-    {
+    double obwod() {
         obwod = bok1 + bok2 + bok3;
         return obwod;
     }
 
-    double poleTrojkata()
-    {
+    double pole() {
         double p;
-        p = obwodTrojkata() / 2;
-        pole = Math.sqrt(p*(p-bok1)*(p-bok2)*(p-bok3));
+        p = obwod() / 2;
+        pole = Math.sqrt(p * (p - bok1) * (p - bok2) * (p - bok3));
+        return pole;
+    }
+}
+
+class Romb extends Figury {
+
+    public Romb(int przekatna1, int przekatna2) {
+        super(przekatna1, przekatna2);
+    }
+    double pole()
+    {
+        pole = (przekatna1 * przekatna2) / 2;
         return pole;
     }
 
-
-    public static void main(String[] args)
+    double bokRombu ()
     {
-        Figury figura = new Figury(8,6);
-
+        double bokRobu;
+        bokRobu = Math.sqrt(Math.pow((bok1 / 2), 2) + Math.pow((bok2 / 2), 2));
+        return bokRobu;
     }
 }
